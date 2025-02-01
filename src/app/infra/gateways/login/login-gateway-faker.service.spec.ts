a89c532ca4027ed0b5c756aff03a16104058d706
Lucas Castro <lfsc09@gmail.com>
2025-02-01 12:24:29 -0300

    ci(github-actions): added first `ci-test.yaml` for running Angular tests (#6)
    This hotfix is adding the `angular-ci-test-workflow` to the repository.
    
    Check commits:
    
    - ci(github-actions): added first `ci-test.yaml` for running Angular tests
    - ci(github-actions): fixed syntax error on file
    - ci(github-actions): dropped `colbyhill21` action for Angular, using manually now
    - ci(github-actions): removed unnecessary steps
    - chore: added `faker` configuration to angular
        - Added a `faker` cofiguration to `angular.json` so that it have 3 types of execution: `production`, `development` and `faker`, where first two will use a httpClient Gateway and the last one the Fake Gateway to run the project without a Database.
    - test(login-gateway-service): added `done` so that the test don't warn about no expectations

--- src/app/infra/gateways/login/login-gateway-faker.service.spec.ts
+++ src/app/infra/gateways/login/login-gateway-faker.service.spec.ts
@@ -13,23 +13,33 @@ describe('LoginGatewayFakerService', () => {
         service = TestBed.inject(LoginGatewayFakerService);
     });
 
-    it('Should authenticate a User', () => {
+    it('Should authenticate a User', (done: DoneFn) => {
         const user = LoginFakerData.users[0];
         const input = { email: user.email, password: user.password } as AuthenticateRequest;
         service.setFailRequest(false);
-        service.authenticate(input).subscribe({
-            next: (response) => {
-                expect(response).toBeTrue();
-            },
+        service.authenticate(input).subscribe((response) => {
+            expect(response).toBeTrue();
+            done();
         });
     });
 
-    it('Should not authenticate a User', () => {
+    it('Should not authenticate a User', (done: DoneFn) => {
         const input = { email: 'abc@gmail.com', password: '000000' } as AuthenticateRequest;
         service.setFailRequest(false);
+        service.authenticate(input).subscribe((response) => {
+            expect(response).toBeFalse();
+            done();
+        });
+    });
+
+    it('Should fail to authenticate a User', (done: DoneFn) => {
+        const user = LoginFakerData.users[0];
+        const input = { email: user.email, password: user.password } as AuthenticateRequest;
+        service.setFailRequest(true);
         service.authenticate(input).subscribe({
-            next: (response) => {
-                expect(response).toBeFalse();
+            error: (error) => {
+                expect(error.message).toBe('Request Failed');
+                done();
             },
         });
     });
