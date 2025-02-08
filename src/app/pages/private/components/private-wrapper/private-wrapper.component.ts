39a8ef27bf9e7fa479d9d224394e8351a263e435
Lucas Castro <lfsc09@gmail.com>
2025-02-07 22:30:32 -0300

    refactor(private-wrapper): updated structure to acommodate `nav-side`

--- src/app/pages/private/components/private-wrapper/private-wrapper.component.ts
+++ src/app/pages/private/components/private-wrapper/private-wrapper.component.ts
@@ -3,14 +3,18 @@ import { Router, RouterOutlet } from '@angular/router';
 import { EMPTY, interval, Subscription, switchMap, takeWhile } from 'rxjs';
 import { environment } from '../../../../../environments/environment';
 import { IdentityService } from '../../../../infra/services/identity/identity.service';
+import { NavSideComponent } from '../nav-side/nav-side.component';
+import { NavSideService } from '../nav-side/nav-side.service';
 import { NavTopComponent } from '../nav-top/nav-top.component';
 
 @Component({
     selector: 'app-private-wrapper',
-    imports: [RouterOutlet, NavTopComponent],
+    imports: [RouterOutlet, NavTopComponent, NavSideComponent],
     template: `
-        <app-nav-top />
-        <router-outlet />
+        <app-nav-side>
+            <app-nav-top />
+            <router-outlet />
+        </app-nav-side>
     `,
     styles: `
         :host {
@@ -23,15 +27,15 @@ import { NavTopComponent } from '../nav-top/nav-top.component';
             color: var(--mat-sys-on-background);
         }
     `,
-    // host: {
-    //     '(document:keyup.Control.;)': 'navModulesService.handleOpen()',
-    // },
+    host: {
+        '(document:keyup.Control.;)': 'navSideService.handleCollapse()',
+    },
 })
 export class PrivateWrapperComponent implements OnInit, OnDestroy {
     /**
      * SERVICES
      */
-    // protected readonly navModulesService = inject(NavModulesService);
+    protected readonly navSideService = inject(NavSideService);
     private readonly _identityService = inject(IdentityService);
     private readonly _routerService = inject(Router);
 
