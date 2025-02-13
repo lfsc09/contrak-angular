47a4e809328c1fb231b70f895f0dd1ec05c7c132
Lucas Castro <lfsc09@gmail.com>
2025-02-12 22:44:21 -0300

    chore: added `contracts` gateway to env to be injected

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,14 +1,16 @@
+import { ContractsGatewayFakerService } from '../app/infra/gateways/contracts/contracts-gateway-faker.service';
 import { LoginGatewayFakerService } from '../app/infra/gateways/login/login-gateway-faker.service';
 
 export const environment = {
-	production: true,
-	title: 'Contrak',
-	host: 'contrak',
-	token: {
-		// lifespan (miliseconds) (1 day)
-		lifespan: 60 * 60 * 24 * 1000,
-		// re-process token interval (miliseconds) (5 min)
-		interval: 5 * 60 * 1000,
-	},
-	loginGateway: LoginGatewayFakerService,
+    production: true,
+    title: 'Contrak',
+    host: 'contrak',
+    token: {
+        // lifespan (miliseconds) (1 day)
+        lifespan: 60 * 60 * 24 * 1000,
+        // re-process token interval (miliseconds) (5 min)
+        interval: 5 * 60 * 1000,
+    },
+    loginGateway: LoginGatewayFakerService,
+    contractsGateway: ContractsGatewayFakerService,
 };
